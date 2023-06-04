65377d8bbf8bb25f3e2444dbd052144f4ca2f39b
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-06-04 22:18:24 +0200

    feat(components): Add Janken game

--- BlazorJanken/Program.cs
+++ BlazorJanken/Program.cs
@@ -1,6 +1,6 @@
+using BlazorJanken;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorJanken;
 using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -16,16 +16,4 @@ builder.Logging.AddSerilog(dispose: true);
 builder.Services.AddSingleton(Log.Logger);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-
-try
-{
-    Log.Information("Starting Blazor Wasm Rock Paper Scissors");
-    await app.RunAsync();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "An exception occurred while creating the WASM host");
-    throw;
-}
+await builder.Build().RunAsync();
