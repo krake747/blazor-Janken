0698006b5322d6cfc1505863a35c506dcd06b5d1
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-06-04 20:15:59 +0200

    feat(di): Setup logging

--- BlazorJanken/Program.cs
+++ BlazorJanken/Program.cs
@@ -1,11 +1,31 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorJanken;
+using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Logging.AddSerilog(dispose: true);
+
+builder.Services.AddSingleton(Log.Logger);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+
+try
+{
+    Log.Information("Starting Blazor Wasm Rock Paper Scissors");
+    await app.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An exception occurred while creating the WASM host");
+    throw;
+}
